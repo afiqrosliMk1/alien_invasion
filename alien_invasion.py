a3bb7945e23be2548f6d71d5b25824fd3e9b33fb
afiq acer v nitro <50091836+afiqrosliMk1@users.noreply.github.com>
2024-05-12 21:30:25 +0800

    add code for fullscreen. uncomment to use it

--- alien_invasion.py
+++ alien_invasion.py
@@ -15,8 +15,17 @@ class AlienInvasion:
         self.clock = pygame.time.Clock()
         self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((self.settings.screen_width , self.settings.screen_height))
+        # FULLSCREEN #
+        # # First we pass (0, 0) so pygame can figure out screen size that can fit fullscreen
+        # self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        # # We update width and height after screen is created
+        # self.settings.screen_width = self.screen.get_rect().width
+        # self.settings.screen_height = self.screen.get_rect().height
+        # WINDOWED #
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        
         pygame.display.set_caption("Alien Invasion")
+
 
         # Set the background color.
         self.bg_color = self.settings.bg_color
