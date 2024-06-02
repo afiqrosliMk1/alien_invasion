0a27754aceeaec244b24732d50d927197481cb5a
afiq acer v nitro <50091836+afiqrosliMk1@users.noreply.github.com>
2024-06-02 21:46:31 +0800

    make aliens drops when reach edges

--- alien_invasion.py
+++ alien_invasion.py
@@ -114,7 +114,8 @@ class AlienInvasion:
         #print(len(self.bullets))
 
     def _update_aliens(self):
-        """Update the position of all aliens in the fleet"""
+        """Check if the fleet is at an edge, then update positions"""
+        self._check_fleet_edges()
         self.aliens.update()
 
     def _create_fleet(self):
@@ -151,6 +152,19 @@ class AlienInvasion:
         new_alien.rect.y = y_position
 
         self.aliens.add(new_alien)
+
+    def _check_fleet_edges(self):
+        """Respond appropriately if any aliens have reached an edge"""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        """Drop the entire fleet and change the fleet's direction"""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen"""
