91fb98e7263f201a66e369d1884d1ee461ff08aa
afiq acer v nitro <50091836+afiqrosliMk1@users.noreply.github.com>
2024-08-18 21:46:15 +0800

    display level

--- scoreboard.py
+++ scoreboard.py
@@ -17,6 +17,7 @@ class Scoreboard:
         # Prepare the initial score image
         self.prep_score()
         self.prep_high_score()
+        self.prep_level()
 
     def prep_score(self):
         """Turn the score into a rendered image."""
@@ -40,13 +41,24 @@ class Scoreboard:
         self.high_score_rect.centerx = self.screen_rect.centerx
         self.high_score_rect.top = self.score_rect.top
 
+    def prep_level(self):
+        """Turn the level into a rendered image."""
+        level_str = f"lv {self.stats.level}"
+        self.level_image = self.font.render(level_str, True, self.text_color, self.settings.bg_color)
+
+        # Display level at top left of the screen
+        self.level_rect = self.level_image.get_rect()
+        self.level_rect.left = self.screen_rect.left + 20
+        self.level_rect.top = 20
+
     def show_score(self):
         """Draw score to the screen"""
         self.screen.blit(self.score_image, self.score_rect)
         self.screen.blit(self.high_score_image, self.high_score_rect)
+        self.screen.blit(self.level_image, self.level_rect)
 
     def check_high_scores(self):
         """Check to see if there's a new high score"""
         if self.stats.score > self.stats.high_score:
             self.stats.high_score = self.stats.score
-            self.prep_high_score
+            self.prep_high_score()
