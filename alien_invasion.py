eb69eb053cf352e38aa8306d299aab5b21ff2e2d
afiq acer v nitro <50091836+afiqrosliMk1@users.noreply.github.com>
2024-08-18 12:02:04 +0800

    update scoring

--- alien_invasion.py
+++ alien_invasion.py
@@ -167,6 +167,10 @@ class AlienInvasion:
         #  The two True arguments tell pygame to delete bullets and aliens that collided
         collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
 
+        if collisions:
+            self.stats.score += self.settings.alien_points
+            self.sb.prep_score()
+            
         if not self.aliens:
             # Destroy existing bullets and create new fleet.
             self.bullets.empty()
